b26ea9a7f1006996035c6bed514c389e68f21477
chuyin221111 <2974853312@qq.com>
2023-06-09 23:50:43 +0800

    set practice2

---
+++ day01/src/StringBuilderDemo.java
@@ -0,0 +1,10 @@
+public class StringBuilderDemo {
+    public static void main(String[] args) {
+        StringBuilder sb=new StringBuilder();
+        System.out.println("sb:"+sb);
+        StringBuilder sb2=new StringBuilder("hello");
+        System.out.println("sb2:"+sb2);
+        System.out.println("sb2:"+sb2.length());
+
+    }
+}
