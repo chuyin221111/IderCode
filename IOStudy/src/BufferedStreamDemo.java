18c91d26f66eee4f86bec3fb2cbaef107053aa92
chuyin221111 <2974853312@qq.com>
2023-06-26 21:06:15 +0800

    IO practice

---
+++ IOStudy/src/BufferedStreamDemo.java
@@ -0,0 +1,25 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+/**
+ * @auther chuyin
+ * @date 2023/6/26
+ * @project java SE
+ */
+public class BufferedStreamDemo {
+    public static void main(String[] args) throws IOException {
+     /*   BufferedWriter bw=new BufferedWriter(new FileWriter("D:\\myStream\\bw.txt"));
+        bw.write("hello\n");
+        bw.write("javaee");
+        bw.close();
+
+      */
+        BufferedReader br=new BufferedReader(new FileReader("D:\\myStream\\bw.txt"));
+        int ch;
+        while((ch=br.read())!=-1)
+        {
+            System.out.print((char)ch);
+        }
+    }
+}
