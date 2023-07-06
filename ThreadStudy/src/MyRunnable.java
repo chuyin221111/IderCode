3d1d46f84c698c177403ff23227138b26f18732a
chuyin221111 <2974853312@qq.com>
2023-07-06 23:59:15 +0800

    Thread practice

--- ThreadStudy/src/MyRunnable.java
+++ ThreadStudy/src/MyRunnable.java
@@ -4,6 +4,10 @@
  * @project java SE
  */
 public class MyRunnable  implements Runnable {
+    /**
+     * MyRunnable
+     *实现Runnable接口，并没有继承Thread,好处是将来可以有自己的父类，而继承Thread也只是为run方法
+     */
     public void run()
     {
         for(int i=0;i<100;i++)
--- ThreadStudy/src/MyRunnableDemo.java
+++ ThreadStudy/src/MyRunnableDemo.java
@@ -6,6 +6,12 @@
 public class MyRunnableDemo{
     public static void main(String[] args) {
         MyRunnable my=new MyRunnable();
+        /**
+         * 可以把MyRunnable类的对象看作同一个资源，有多个线程（t1,t2）去使用
+         */
+        //Thread （Runnable target）
+        //Thread ti1=new Thread(my);
+        //Thread(Runnable target,String name)
         Thread t1=new Thread(my,"高铁");
         Thread t2=new Thread(my,"飞机");
         t1.start();
