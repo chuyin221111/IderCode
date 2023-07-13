220acbf8bf589007d5c70eb305e538ffa0de44aa
chuyin221111 <2974853312@qq.com>
2023-07-13 23:58:22 +0800

    Final exam practice

--- IOStudy/src/Box.java
+++ IOStudy/src/Box.java
@@ -5,13 +5,27 @@
  */
 public class Box {
     private int milk;
-    public void put(int milk)
-    {
+    private boolean state=false;
+
+    public void put(int milk) throws InterruptedException {
+        if(state)
+        {
+            wait();
+        }
         this.milk=milk;
         System.out.println("送奶工将第"+this.milk+"瓶奶放入奶瓶");
+        state=true;
     }
     public void get()
+    {  if(!state)
     {
+        try {
+            wait();
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
         System.out.println("用户拿到第"+this.milk+"瓶奶");
+        state=false;
     }
 }
