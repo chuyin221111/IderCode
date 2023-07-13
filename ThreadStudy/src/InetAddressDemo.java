f808a82bae2fc593732bfb62298dea4ab256514f
chuyin221111 <2974853312@qq.com>
2023-07-13 23:36:31 +0800

    Final exam practice

---
+++ ThreadStudy/src/InetAddressDemo.java
@@ -0,0 +1,18 @@
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
+/**
+ * @auther chuyin
+ * @date 2023/7/13
+ * @project java SE
+ */
+public class InetAddressDemo {
+    public static void main(String[] args) throws UnknownHostException {
+        InetAddress address = InetAddress.getByName("192.168.1.66");
+        String name=address.getHostName();
+        String ip=address.getHostAddress();
+        System.out.println("主机名"+name);
+        System.out.println("Ip地址"+ip);
+
+    }
+}
