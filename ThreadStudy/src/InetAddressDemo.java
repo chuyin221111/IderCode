2655004c69b809cc9159aeefccbce41a147cfe3c
chuyin221111 <2974853312@qq.com>
2023-07-16 00:25:21 +0800

    UDP practice

--- ThreadStudy/src/InetAddressDemo.java
+++ ThreadStudy/src/InetAddressDemo.java
@@ -8,7 +8,7 @@ import java.net.UnknownHostException;
  */
 public class InetAddressDemo {
     public static void main(String[] args) throws UnknownHostException {
-        InetAddress address = InetAddress.getByName("192.168.1.66");
+        InetAddress address = InetAddress.getByName("172.22.188.179");
         String name=address.getHostName();
         String ip=address.getHostAddress();
         System.out.println("主机名"+name);
