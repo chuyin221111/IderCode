4e78974b5c2a4ffe2d95ce9d14552ab21fdf1e79
chuyin221111 <2974853312@qq.com>
2023-07-26 21:25:05 +0800

    UDP practice

--- UDPTCPStudy/src/SendDemo.java
+++ UDPTCPStudy/src/SendDemo.java
@@ -8,11 +8,24 @@ import java.net.InetAddress;
  * @date 2023/7/16
  * @project java SE
  */
+
+/**
+ * UDP发送数据步骤
+ * 1.创建发送端的Socket对象（DatagramSocket）
+ * 2.创建数据，并把数据打包
+ * 3.调用DatagramSocket对象的方法发送数据
+ * 4.关闭发送端
+ */
 public class SendDemo {
     public static void main(String[] args) throws IOException {
+        //1.创建发送端的Socket对象（DatagramSocket）
+        //DatagramSocket() 构造数据报套接字并将其绑到本地主机上的任何可用端口
         DatagramSocket ds=new DatagramSocket();
+        //.创建数据，并把数据打包
+        //DatagramPacket(byte[] buf,int length,InetAddress address, int port)
+        //构造一个数据包，发送长度为length的数据包到指定主机上的指定端口号
         byte[] bys="hello,udp,我来了".getBytes();
-        DatagramPacket dp=new DatagramPacket(bys,bys.length, InetAddress.getByName("172.22.188.179"),10086);
+        DatagramPacket dp=new DatagramPacket(bys,bys.length, InetAddress.getByName("192.168.0.3"),10086);
         ds.send(dp);
         ds.close();
     }
