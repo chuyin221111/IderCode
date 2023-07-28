d9aaad70faf8dfbc9100b4ea898ee064580861a2
chuyin221111 <2974853312@qq.com>
2023-07-28 23:25:28 +0800

    TCP practice

--- UDPTCPStudy/src/ClientDemo.java
+++ UDPTCPStudy/src/ClientDemo.java
@@ -8,9 +8,17 @@ import java.net.Socket;
  * @date 2023/7/17
  * @project java SE
  */
+/*
+   TCP发送数据的步骤
+   1.创建客户端的Socket对象（Socket）
+   2.获取输出流，写数据
+     OutputStream
+   3.释放资源
+ */
 public class ClientDemo {
     public static void main(String[] args) throws IOException {
-        Socket s=new Socket("172.22.188.179",10000);
+        //创建客户端
+        Socket s=new Socket("192.168.1.7",10001);
         OutputStream os=s.getOutputStream();
         os.write("hello,tcp,我来了".getBytes());
         InputStream is=s.getInputStream();
@@ -18,6 +26,8 @@ public class ClientDemo {
         int len=is.read(bys);
         String data=new String(bys,0,len);
         System.out.println("客户端："+data);
+
+
         s.close();
     }
 }
--- UDPTCPStudy/src/ServerDemo.java
+++ UDPTCPStudy/src/ServerDemo.java
@@ -1,6 +1,5 @@
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.OutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
 
@@ -9,17 +8,27 @@ import java.net.Socket;
  * @date 2023/7/17
  * @project java SE
  */
+/*
+   TCP接收数据的步骤
+   1.创建服务器端的Socket对象（ServerSocket）
+   2.监听客户端连接，返回一个Socket对象
+   3.获取输入流，读数据，并把数据显示在控制台
+   4.释放资源
+ */
 public class ServerDemo {
     public static void main(String[] args) throws IOException {
-        ServerSocket ss=new ServerSocket(10000);
+        //创建服务端
+        //直接创建Socket对象，不能形成虚拟链路
+        ServerSocket ss=new ServerSocket(10001);
+        //Socket accept():监听要连接的客户端，获取套接字
         Socket s=ss.accept();
+        //获取输入流，读数据，显示在控制台
         InputStream is=s.getInputStream();
         byte[] bys=new byte[1024];
         int len=is.read(bys);
         String data=new String(bys,0,len);
         System.out.println("服务器："+data);
-        OutputStream os=s.getOutputStream();
-        os.write("数据已收到".getBytes());
+        //释放资源
         s.close();
         ss.close();
     }
