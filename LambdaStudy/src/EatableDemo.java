09d79023b32ac081695e8c6041e79a88f69a7918
chuyin221111 <2974853312@qq.com>
2023-08-03 23:26:27 +0800

    Lambda practice

---
+++ LambdaStudy/src/EatableDemo.java
@@ -0,0 +1,15 @@
+/**
+ * @auther chuyin
+ * @date 2023/8/3
+ * @project java SE
+ */
+public class EatableDemo {
+    public static void main(String[] args) {
+    Eatable e=new Eatablelmp();
+     useEatable();
+    }
+    private static void useEatable(Eatable e)
+    {
+     e.eat();
+    }
+}
---
+++ LambdaStudy/src/Eatablelmp.java
@@ -0,0 +1,12 @@
+/**
+ * @auther chuyin
+ * @date 2023/8/3
+ * @project java SE
+ */
+public class Eatablelmp implements Eatable
+{
+    public void eat()
+    {
+        System.out.println("一天一苹果，医生远离我");
+    }
+}
